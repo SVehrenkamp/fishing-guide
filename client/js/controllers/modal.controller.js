24f642188b8995a8f10fe315fa096db14ef745cd
Scott.Vehrenkamp <scott.vehrenkamp@target.com>
2015-02-06 09:12:02 -0600

    added fish logic to trip.session and updated sass file.

--- client/js/controllers/modal.controller.js
+++ client/js/controllers/modal.controller.js
@@ -1,7 +1,16 @@
 app.controller('ModalController', function($scope, $modalInstance, addFish){
+	var fish = {
+		'species' : 'bluegill',
+		'size' : "8in",
+		'lure' : {
+			'type': 'jig',
+			'size': '1/64oz',
+			'color': 'glow red'
+		}
+	};
 	$scope.addFish = addFish;
 	$scope.ok = function(){
-		$scope.addFish();
+		$scope.addFish(fish);
 		$modalInstance.dismiss();
 		console.log('Fish Added!');
 	}
